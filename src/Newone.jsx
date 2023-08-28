928c9e9c671f63ad01e49c43f86c463da929e4ff
Naveensarilla <naveen.s@egradtutor.in>
2023-08-28 15:22:28 +0530

    Done converting google img to react

---
+++ src/Newone.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useState } from 'react';
+import axios from 'axios'
+
+export const Newone = () => {
+
+    const [file,setFile] = useState("");
+    const setimgfile = (e)=>{
+        setFile(e.target.files[0])
+    }
+
+    const onSubmit = (e) => {
+        const formData = new FormData();
+        formData.append("Files",file)
+        e.preventDefault();
+        // const config = {
+        //     headers:{
+        //         "Content-Type":"multipart/form-data"
+        //     }
+        // }
+         axios.post('http://localhost:501/upload', formData, {
+            method: 'POST',
+            body: formData,
+
+        }).catch(res => {console.log(file)})
+        .catch(error => {console.log(`Error ${error}`)})
+      
+
+    }
+  return (
+    <div>
+        <form onSubmit={onSubmit} encType='multipart/form-data'>
+            <div class="custom-file-input">
+                <input id="fileInput" type="file" name="Files"    onChange={setimgfile}/>
+                {/* <label for="fileInput" id="fileInputLabel">Select Files</label> */}
+            </div>
+            <button id="myButton" type="submit">Submit</button>
+        </form> 
+
+        <img src="https://drive.google.com/uc?export=view&id=1FmI2WRr3zQTz4Nj3e_OfIH8cgFhDa-W1" width={200} alt="" />
+    </div>
+  )
+}
